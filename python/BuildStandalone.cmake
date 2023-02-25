1012050febac3ca5b2a7c6db43d1ec6abaf181ca
jacobkahn <jacobkahn1@gmail.com>
2023-02-24 18:11:00 -0800

    Transition to scikit build Python for build

--- python/BuildStandalone.cmake
+++ python/BuildStandalone.cmake
@@ -1,4 +1,4 @@
-cmake_minimum_required(VERSION 3.1)
+cmake_minimum_required(VERSION 3.4)
 
 file(GLOB
   KENLM_PYTHON_STANDALONE_SRCS
@@ -12,30 +12,70 @@ list(FILTER KENLM_PYTHON_STANDALONE_SRCS EXCLUDE REGEX ".*main.cc")
 list(FILTER KENLM_PYTHON_STANDALONE_SRCS EXCLUDE REGEX ".*test.cc")
 
 add_library(
-  kenlm
+  kenlm_lib
   SHARED
   ${KENLM_PYTHON_STANDALONE_SRCS}
   )
 
-target_include_directories(kenlm PRIVATE ${PROJECT_SOURCE_DIR})
-target_compile_definitions(kenlm PRIVATE KENLM_MAX_ORDER=${KENLM_MAX_ORDER})
+target_include_directories(kenlm_lib PRIVATE ${PROJECT_SOURCE_DIR})
+target_compile_definitions(kenlm_lib PRIVATE KENLM_MAX_ORDER=${KENLM_MAX_ORDER})
 
 find_package(ZLIB)
 find_package(BZip2)
 find_package(LibLZMA)
 
 if (ZLIB_FOUND)
-  target_link_libraries(kenlm PRIVATE ${ZLIB_LIBRARIES})
-  target_include_directories(kenlm PRIVATE ${ZLIB_INCLUDE_DIRS})
-  target_compile_definitions(kenlm PRIVATE HAVE_ZLIB)
+  target_link_libraries(kenlm_lib PRIVATE ${ZLIB_LIBRARIES})
+  target_include_directories(kenlm_lib PRIVATE ${ZLIB_INCLUDE_DIRS})
+  target_compile_definitions(kenlm_lib PRIVATE HAVE_ZLIB)
 endif()
 if(BZIP2_FOUND)
-  target_link_libraries(kenlm PRIVATE ${BZIP2_LIBRARIES})
-  target_include_directories(kenlm PRIVATE ${BZIP2_INCLUDE_DIR})
-  target_compile_definitions(kenlm PRIVATE HAVE_BZLIB)
+  target_link_libraries(kenlm_lib PRIVATE ${BZIP2_LIBRARIES})
+  target_include_directories(kenlm_lib PRIVATE ${BZIP2_INCLUDE_DIR})
+  target_compile_definitions(kenlm_lib PRIVATE HAVE_BZLIB)
 endif()
 if(LIBLZMA_FOUND)
-  target_link_libraries(kenlm PRIVATE ${LIBLZMA_LIBRARIES})
-  target_include_directories(kenlm PRIVATE ${LIBLZMA_INCLUDE_DIRS})
-  target_compile_definitions(kenlm PRIVATE HAVE_LZMA)
+  target_link_libraries(kenlm_lib PRIVATE ${LIBLZMA_LIBRARIES})
+  target_include_directories(kenlm_lib PRIVATE ${LIBLZMA_INCLUDE_DIRS})
+  target_compile_definitions(kenlm_lib PRIVATE HAVE_LZMA)
 endif()
+
+# set output name of the kenlm_lib --> kenlm
+set_target_properties(
+  kenlm_lib
+  PROPERTIES
+  OUTPUT_NAME "kenlm"
+)
+
+# Build the Python library
+add_library(kenlm MODULE ${PROJECT_SOURCE_DIR}/python/kenlm.cpp)
+
+target_link_libraries(kenlm kenlm_lib)
+target_include_directories(kenlm PRIVATE ${PROJECT_SOURCE_DIR})
+target_compile_definitions(kenlm PRIVATE KENLM_MAX_ORDER=${KENLM_MAX_ORDER})
+
+# provided by scikit-build
+find_package(PythonExtensions REQUIRED)
+python_extension_module(kenlm)
+# find_package(Cython REQUIRED)
+# add_cython_target(${PROJECT_SOURCE_DIR}/python/kenlm.pyx)
+
+install(TARGETS kenlm kenlm_lib DESTINATION ${PYTHON_RELATIVE_SITE_PACKAGES_DIR})
+
+# rpath setting to make sure the python binding library finds
+# the primary lib
+if(APPLE)
+  # macOS
+  set(CMAKE_MACOSX_RPATH ON)
+  set(_portable_rpath_origin "@loader_path")
+else()
+  # Linux
+  set(CMAKE_BUILD_RPATH_USE_ORIGIN ON)
+  set(_portable_rpath_origin $ORIGIN)
+endif(APPLE)
+
+set_target_properties(kenlm PROPERTIES
+  BUILD_RPATH ${_portable_rpath_origin}
+  INSTALL_RPATH ${_portable_rpath_origin}
+  INSTALL_RPATH_USE_LINK_PATH TRUE
+  )
--- setup.py
+++ setup.py
@@ -7,116 +7,13 @@ import os
 import sys
 import re
 from pathlib import Path
+from skbuild import setup
+import skbuild
 
-#Does gcc compile with this header and library?
-def compile_test(header, library):
-    dummy_path = os.path.join(os.path.dirname(__file__), "dummy")
-    command = "bash -c \"g++ -include " + header + " -l" + library + " -x c++ - <<<'int main() {}' -o " + dummy_path + " >/dev/null 2>/dev/null && rm " + dummy_path + " 2>/dev/null\""
-    return os.system(command) == 0
-
-max_order = "6"
-is_max_order = [s for s in sys.argv if "--max_order" in s]
-for element in is_max_order:
-    max_order = re.split('[= ]',element)[1]
-    sys.argv.remove(element)
-
-FILES = glob.glob('util/*.cc') + glob.glob('lm/*.cc') + glob.glob('util/double-conversion/*.cc') + glob.glob('python/*.cc')
-FILES = [fn for fn in FILES if not (fn.endswith('main.cc') or fn.endswith('test.cc'))]
-
-#We don't need -std=c++11 but python seems to be compiled with it now.  https://github.com/kpu/kenlm/issues/86
-ARGS = ['-O3', '-DNDEBUG', '-DKENLM_MAX_ORDER='+max_order, '-std=c++11']
-INCLUDE_PATHS = []
-
-if platform.system() == 'Linux':
-    LIBS = ['stdc++', 'rt']
-    ARGS.append('-DHAVE_CLOCKGETTIME')
-elif platform.system() == 'Darwin':
-    LIBS = ['c++']
-else:
-    LIBS = []
-
-#Attempted fix to https://github.com/kpu/kenlm/issues/186 and https://github.com/kpu/kenlm/issues/197
-if platform.system() == 'Darwin':
-    ARGS += ["-stdlib=libc++", "-mmacosx-version-min=10.7"]
-    INCLUDE_PATHS.append("/usr/local/include")
-
-if compile_test('zlib.h', 'z'):
-    ARGS.append('-DHAVE_ZLIB')
-    LIBS.append('z')
-
-if compile_test('bzlib.h', 'bz2'):
-    ARGS.append('-DHAVE_BZLIB')
-    LIBS.append('bz2')
-
-if compile_test('lzma.h', 'lzma'):
-    ARGS.append('-DHAVE_XZLIB')
-    LIBS.append('lzma')
-
-
-class build_ext(_build_ext):
-    def run(self):
-        try:
-            out = subprocess.check_output(["cmake", "--version"])
-        except OSError:
-            raise RuntimeError(
-                "CMake must be installed to build the following extensions: "
-                + ", ".join(e.name for e in self.extensions)
-            )
-
-        ext_dir = str(Path(self.get_ext_fullpath('libkenlm')).absolute().parent)
-        source_dir = str(Path(__file__).absolute().parent)
-
-        cmake_args = [
-            "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=" + ext_dir,
-            "-DBUILD_SHARED_LIBS=ON",
-            "-DBUILD_PYTHON_STANDALONE=ON",
-        ]
-        cfg = "Debug" if self.debug else "Release"
-        build_args = ["--config", cfg]
-
-        if platform.system() == "Windows":
-            cmake_args += [
-                "-DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON",
-                "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY_{}={}".format(cfg.upper(), ext_dir),
-                "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}".format(cfg.upper(), ext_dir),
-                "-DCMAKE_ARCHIVE_OUTPUT_DIRECTORY_{}={}".format(cfg.upper(), ext_dir),
-            ]
-            if sys.maxsize > 2**32:
-                cmake_args += ["-A", "x64"]
-            build_args += ["--", "/m"]
-        else:
-            cmake_args += ["-DCMAKE_BUILD_TYPE=" + cfg]
-            build_args += ["--", "-j4"]
-
-        env = os.environ.copy()
-        env["CXXFLAGS"] = '{} -fPIC -DVERSION_INFO=\\"{}\\"'.format(
-            env.get("CXXFLAGS", ""), self.distribution.get_version()
-        )
-
-        if not os.path.exists(self.build_temp):
-            os.makedirs(self.build_temp)
-        subprocess.check_call(
-            ["cmake", source_dir] + cmake_args, cwd=self.build_temp, env=env
-        )
-        subprocess.check_call(
-            ["cmake", "--build", "."] + build_args, cwd=self.build_temp
-        )
-
-        return _build_ext.run(self)
-
-
-ext_modules = [
-    Extension(name='kenlm',
-        sources=FILES + ['python/kenlm.cpp'],
-        language='C++', 
-        include_dirs=['.'] + INCLUDE_PATHS,
-        libraries=LIBS, 
-        extra_compile_args=ARGS),
-]
-
+# See python/BuildStandalone.cmake, which builds Python libs
 setup(
-    name='kenlm',
-    ext_modules=ext_modules,
-    cmdclass={"build_ext": build_ext},
-    include_package_data=True,
+    name="kenlm",
+    cmake_args=['-DBUILD_PYTHON_STANDALONE:BOOL=ON'],
+    packages=['kenlm'],
+    package_dir={"kenlm": ""},
 )
