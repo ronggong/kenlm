c856b2f675241f8432e0dae4895a13bc27cd1e7c
jacobkahn <jacobkahn1@gmail.com>
2023-02-25 20:21:56 -0800

    Try an empty output name for the lib prop

--- python/BuildStandalone.cmake
+++ python/BuildStandalone.cmake
@@ -40,21 +40,29 @@ if(LIBLZMA_FOUND)
   target_compile_definitions(kenlm_lib PRIVATE HAVE_LZMA)
 endif()
 
-# set output name of the kenlm_lib --> kenlm
+# set output name of the kenlm --> kenlm
 set_target_properties(
   kenlm_lib
   PROPERTIES
   OUTPUT_NAME "kenlm"
 )
 
+# find_package(PythonExtensions REQUIRED)
+# find_package(Cython REQUIRED)
+
+# provided by scikit-build
+# add_cython_target(${PROJECT_SOURCE_DIR}/python/kenlm CXX)
+# add_library(kenlm MODULE ${kenlm})
+# python_extension_module(kenlm)
+
 # Build the Python library
 add_library(kenlm MODULE ${PROJECT_SOURCE_DIR}/python/kenlm.cpp)
 
-# set output name of the kenlm_lib --> kenlm
+# set output name of the kenlm --> kenlm
 set_target_properties(
   kenlm
   PROPERTIES
-  OUTPUT_NAME "kenlm"
+  OUTPUT_NAME ""
 )
 
 
@@ -62,13 +70,13 @@ target_link_libraries(kenlm kenlm_lib)
 target_include_directories(kenlm PRIVATE ${PROJECT_SOURCE_DIR})
 target_compile_definitions(kenlm PRIVATE KENLM_MAX_ORDER=${KENLM_MAX_ORDER})
 
-# provided by scikit-build
 find_package(PythonExtensions REQUIRED)
 python_extension_module(kenlm)
 # find_package(Cython REQUIRED)
 # add_cython_target(${PROJECT_SOURCE_DIR}/python/kenlm.pyx)
 
-install(TARGETS kenlm kenlm_lib DESTINATION ${PYTHON_RELATIVE_SITE_PACKAGES_DIR})
+install(TARGETS kenlm_lib kenlm DESTINATION ${PYTHON_RELATIVE_SITE_PACKAGES_DIR})
+# install(FILES ${PROJECT_SOURCE_DIR}/python/__init__.py DESTINATION ${PYTHON_RELATIVE_SITE_PACKAGES_DIR}/kenlm)
 
 # rpath setting to make sure the python binding library finds
 # the primary lib
